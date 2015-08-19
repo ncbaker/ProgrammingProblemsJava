99276b2cc07ce10a8a564dd2e587518bd6386063
nathanb <nathancbaker@yahoo.com>
2015-08-19 07:57:13 -0500

    Lowest ancestor

--- src/com/ncb/DataStructures/Trees.java
+++ src/com/ncb/DataStructures/Trees.java
@@ -1,5 +1,9 @@
 package com.ncb.DataStructures;
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Stack;
+
 /**
  Level Order Traversal
       3
@@ -33,11 +37,63 @@ public class Trees {
         return new Node(4, rL, rR);
     }
 
-    /*https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor*/
+    /*https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor
+    * */
     public static Node lca(Node root,int v1,int v2)
     {
+        if(root == null)
+            return new Node();
+
+        Stack<Node> v1Q = GetLineage(root, v1);
+        Stack<Node> v2Q = GetLineage(root, v2);
+
+        while(v1Q.size() > v2Q.size())
+            v1Q.pop();
+        while(v2Q.size() > v1Q.size())
+            v2Q.pop();
+
+        while(v1Q.peek().data != v2Q.peek().data) {
+            v1Q.pop();
+            v2Q.pop();
+        }
+
+        if(v1Q.size() > 0) {
+            System.out.print(v1Q.peek().data);
+            return v1Q.peek();
+        }
+
         return new Node();
     }
+    static Stack<Node> GetLineage(Node root, int value)
+    {
+        Stack<Node> q = new Stack<Node>();
+        q.push(root);
+
+        Node current = root;
+        while(true) {
+            if(value < current.data && current.left != null) {
+                /*if(current.left == null)
+                    break;*/
+
+                q.push(current.left);
+                current = current.left;
+            }
+            else if(value > current.data && current.right != null) {
+                /*if(current.right == null)
+                    break;*/
+
+                q.push(current.right);
+                current = current.right;
+            }
+            else {
+                q.clear();
+                break;
+            }
+        }
+
+        return q;
+    }
+
 
     /*https://www.hackerrank.com/challenges/binary-search-tree-insertion
     * */
@@ -74,6 +130,7 @@ public class Trees {
         }
     }
 
+
     /*https://www.hackerrank.com/challenges/tree-level-order-traversal
     * BFS concept  */
     public static void LevelOrder(Node root)
@@ -91,5 +148,83 @@ public class Trees {
             if(n.right != null)
                 nodeQ.add(n.right);
         }
+    }
+
+
+    /* https://www.hackerrank.com/challenges/tree-top-view */
+    static void top_view(Node root)
+    {
+        if(root.left != null)
+            left_view(root.left);
+
+        System.out.print(root.data + " ");
+
+        if(root.right != null)
+            right_view(root.right);
+    }
+    static void left_view(Node root)
+    {
+        if(root.left != null)
+            left_view(root.left);
+
+        System.out.print(root.data + " ");
+    }
+    static void right_view(Node root)
+    {
+        System.out.print(root.data + " ");
+        if(root.right != null)
+            right_view(root.right);
+    }
+
+
+    /* https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree */
+    static int height(Node root)
+    {
+        if(root.left == null && root.right == null)
+            return 1;
+
+        int leftHeight = 0, rightHeight = 0;
+        if(root.left != null)
+            leftHeight += height(root.left);
+        if(root.right != null)
+            rightHeight += height(root.right);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+
+    /* https://www.hackerrank.com/challenges/tree-inorder-traversal */
+    static void Inorder(Node root) {
+        if(root.left != null) {
+            Inorder(root.left);
+            System.out.print(root.data + " ");
+        }
+        else
+            System.out.print(root.data + " ");
+
+        if(root.right != null)
+            Inorder(root.right);
+    }
+
+
+    /* https://www.hackerrank.com/challenges/tree-postorder-traversal  */
+    static void Postorder(Node root) {
+        if(root.left != null)
+            Postorder(root.left);
+        if(root.right != null)
+            Postorder(root.right);
+
+        System.out.print(root.data + " ");
+    }
+
+
+    /* https://www.hackerrank.com/challenges/tree-preorder-traversal */
+    static void Preorder(Node root) {
+        System.out.print(root.data + " ");
+
+        if(root.left != null)
+            Preorder(root.left);
+        if(root.right != null)
+            Preorder(root.right);
     }
 }
